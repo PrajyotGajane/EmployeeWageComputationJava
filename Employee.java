83b33b6fc8b57c351c6777c0e65e8f39aeb4a8a0
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-31 17:56:51 +0530

    Attendance logic applied

--- Employee.java
+++ Employee.java
@@ -1,5 +1,7 @@
 public class Employee{
 	public static void main(String[] args){
 	System.out.println("Welcome to Employee Wage Computation Program\n");
+	int attendance = (int)((Math.random() * 10) % 2);
+        System.out.println((attendance == 1) ? "Employee Present" : "Employee Absent");
 	}
 }
