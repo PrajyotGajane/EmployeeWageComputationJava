15e00f46f00d9b0262f8b06002e477f41c906394
PrajyotGajane <prajyotgajane@gmail.com>
2020-05-31 19:26:13 +0530

    Code written into main method 2

--- Employee.java
+++ Employee.java
@@ -1,28 +1,29 @@
 public class Employee{
-	public static void main(String[] args){
-		System.out.println("Welcome to Employee Wage Computation Program\n");
-		int attendance = (int)((Math.random() * 10) % 2);
-	        System.out.println((attendance == 1) ? "Employee Present" : "Employee Absent");
-		int workTime = 0;
+    public static void main(String[] args){
+        System.out.println("Welcome to Employee Wage Computation Program\n");
+        int attendance = (int)((Math.random() * 10) % 2);
+        System.out.println((attendance == 1) ? "Employee Present" : "Employee Absent");
+        int workTime = 0;
+        int isFullTime = 8;
+        int isPartTime = 4;
+        if(attendance == 1)
+            workTime = (int)((Math.random() * 10) % 2);
 
-        	if(attendance == 1)
-	            workTime = (int)((Math.random() * 10) % 2);
-	
-        	int work_hours = 0;
+        int work_hours = 0;
 
-	        if(workTime == 1)
-        	    work_hours = 8;
-	        else
-        	    work_hours = 4;
+        if(workTime == 1)
+            work_hours = isFullTime;
+        else
+            work_hours = isPartTime;
 
-	        int wage = 0;
+        int wage = 0;
 
-        	if(attendance == 1){
-	            System.out.println((workTime == 1) ? "Full time" : "Part time");
-        	    wage = 20 * work_hours;
-        	}
-        	System.out.print("Day's wage is :"+wage);
-	}
+        if(attendance == 1){
+            System.out.println((workTime == 1) ? "Full time" : "Part time");
+            wage = 20 * work_hours;
+        }
+        System.out.print("Day's wage is :"+wage);
+    }
 
 }
 
